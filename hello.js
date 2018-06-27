f84d3ba57dcf832bcf3a6f6faafccbc6f8d7fd6a
rajiv-rajaram <rajivsays@hotmail.com>
2018-06-27 22:36:51 +1000

    corretion hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,1 @@
-const a=1
-const b=2
-const c=a+b
-console.log(a,b,c)
-console.log(a+b)
-console.log("My first JS program")
-console.log("this is committed now")
+console.log("My first JS program");
